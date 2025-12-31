4af49ea052ff3920ce5df6d80a65959c8d900cbe
thupten tsundue <thups1951@gmail.com>
2025-12-31 15:17:46 -0500

    added office page

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@ export const SITE_NAME = 'Karuna Law';
 export const SITE_URL = 'https://www.karunalaw.ca';
 
 // For custom domain, BASE_URL should always be '/'
-export const BASE_URL = '';
+export const BASE_URL = '/';
