137e98bd4e4da1c141adc8c2b7617b916dd72d4d
thupten tsundue <thups1951@gmail.com>
2025-12-31 14:26:26 -0500

    added header

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+export const SITE_NAME = 'Karuna Law';
+export const SITE_URL = 'https://www.karunalaw.ca';
+
+// For custom domain, BASE_URL should always be '/'
+export const BASE_URL = '';
---
+++ src/pages/team/index.astro
@@ -0,0 +1,1 @@
+<div>team page</div>
